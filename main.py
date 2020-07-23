d2e5df0a84b990713e74790efb5d537666744ce9
siddharth0703 <siddharth.svr@gmail.com>
2020-07-23 21:53:48 +0530

    update

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from math import hypot
 from random import randint
 
 import pygame
 
+pygame.init()
 # Screen Size
 screen = pygame.display.set_mode((1000, 700))
 running = True
+# Score
+score_value = 0
+font = pygame.font.SysFont('forte', 30)
 
+text_x = 10
+text_y = 10
 # Icon and Title and Background
-pygame.display.set_caption('First App')
+pygame.display.set_caption('Egg and Hen Game')
 icon = pygame.image.load('1.png')
 pygame.display.set_icon(icon)
 background = pygame.image.load('soc.jpg')
@@ -48,12 +55,25 @@ def fire_egg(x, y):
     screen.blit(eggimage, (x + 16, y + 10))
 
 
+def isCollision(egg_x, egg_y, bucket_x, bucket_y):
+    distance = hypot(bucket_y - egg_y, bucket_x - egg_x)
+    if distance < 27:
+        return True
+    return False
+
+
+# Font Score
+def scorecard(x, y):
+    textsurface = font.render('Score : ' + str(score_value), True, (0, 0, 0))
+    screen.blit(textsurface, (0, 0))
+
+
 # GameLoop
 while running:
     # rgb
     screen.fill((0, 255, 0))
     # screen.blit(background, (0, 0))
-    for event in pygame.event.get():
+    for event in pygame.event.get():  # Gathers all events
         if event.type == pygame.QUIT:
             running = False
         # if  key is pressed
@@ -65,7 +85,10 @@ while running:
                 player_x_value_change = 0.7
                 print('Right ARROW KEY IS PRESSED')
             if event.key == pygame.K_SPACE:
-                fire_egg(player_x_axis, egg_y_axis)
+                if egg_state == 'ready':
+                    egg_x_axis = player_x_axis
+                    fire_egg(egg_x_axis, egg_y_axis)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player_x_value_change = 0.0
@@ -87,10 +110,25 @@ while running:
     elif e1_x_axis >= 736:
         enemy_x_value_change = -0.03
         e1_y_axis += enemy_y_value_change
-    # Egg
-    if egg_state is 'fire':
-        fire_egg(player_x_axis, egg_y_axis)
+
+    # Egg(this state runs when SPACE KEY is pressed
+    if egg_y_axis <= 0:
+        egg_y_axis = 480
+        egg_state = 'ready'
+    if egg_state == 'fire':
+        fire_egg(egg_x_axis, egg_y_axis)
         egg_y_axis -= egg_y_value_change
+    # Collision
+    collision = isCollision(egg_x_axis, egg_y_axis, e1_x_axis, e1_y_axis)
+    if collision:
+        egg_state = 'ready'
+        egg_y_axis = 480
+        score_value += 1
+        print("Score ", score_value)
+        e1_x_axis = randint(0, 735)
+        e1_y_axis = randint(120, 240)
+
     player(player_x_axis, player_y_axis)
+    scorecard(text_x, text_y)
     my_enemy(e1_x_axis, e1_y_axis)
     pygame.display.update()
